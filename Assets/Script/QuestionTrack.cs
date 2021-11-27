91f8e6cd228a7ad0189e3b962a613bcc3133b0bc
Maganac33 <palanog.michael@yahoo.com>
2021-11-26 20:10:53 -0600

    Random Question Generator
    Made script to create random questions without the need for multiple questions. Have not implemented Button functionality and question track yet.

---
+++ Assets/Script/QuestionTrack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class QuestionTrack : MonoBehaviour
+{
+    public bool AnswerTrack = false;
+    public QuizManager quizManager;
+
+    
+    public void Answer()
+    {
+        if (AnswerTrack)
+        {
+            Debug.Log("Yes");
+            quizManager.nextTrack();
+        }
+        else
+        {
+            Debug.Log("No");
+            quizManager.nextTrack();
+        }
+    }
+
+}
