42e6aea23fac3cb9bb09d55fa7ede65846109730
duongNguyennn <94706590+duongNguyennn@users.noreply.github.com>
2021-11-26 20:23:09 -0900

    Resolve merge conflicts during revert

--- Assets/Script/Questions.cs
+++
@@ -1,9 +0,0 @@
-﻿[System.Serializable]
-
-public class Questions
-{
-    public string Question;
-    public string option1, option2, option3, option4;
-    public string[] Answers;
-    public int QTrack;
-}
